d4e1d7686608beb0fef115983376454372a41ce7
Chikago13 <sivko2201@gmail.com>
2023-03-04 17:22:36 +0300

    Оптимизация шаблона base

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,22 +14,36 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
-    # text = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data= items)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method =='POST':
+        title = request.form['title']
+        price = request.form['price']
+        description = request.form['description']
+
+        item = Item(title=title, price=price, description=description)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Произошла ошибка"
+    else:
+        return render_template('create.html')
 
 @app.route('/task')
 def task():
