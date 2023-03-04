c26a1160683f363a731d939a78982dc5856c2a06
Chikago13 <sivko2201@gmail.com>
2023-03-04 15:51:25 +0300

    Создание class для БД

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'aqlite:///shop.db'
+db = SQLAlchemy
 
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer(100), nullable=False)
+    isActive = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
 @app.route('/')
 def index():
     return render_template('index.html')
